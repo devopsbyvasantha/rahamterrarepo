341e7f49d3602f4f7c1685472274c54ed7397ca5
devopsbyvasantha <devopsbyvasantha@gmail.com>
2025-10-22 11:46:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "three" {
   count         = 2
-  ami           = "ami-00ca32bbc84273381"
+  ami           = "ami-0341d95f75f311023"
   instance_type = "t2.micro"
 
   tags = {
